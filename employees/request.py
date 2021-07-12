652fb5a28007352687960a5270e6e31e3fead60b
wooldridge12 <wooldridgemikey@gmail.com>
2021-07-12 12:02:02 -0400

    First commit to GitHub

---
+++ employees/request.py
@@ -0,0 +1,35 @@
+EMPLOYEES = [
+    {
+        "id": 1,
+        "name": "Richard",
+        "locationId": 2
+    },
+    {
+      "id": 2,
+      "name": "Davey Jones",
+      "locationId": 2
+    },
+    {
+      "id": 3,
+      "name": "John Basalone",
+      "locationId": 1
+    },
+    {
+      "id": 4,
+      "name": "Lisa Rigdon",
+      "locationId": 1
+    },
+    {
+      "name": "Isabelle",
+      "locationId": 1,
+      "id": 5
+    },
+    {
+      "name": "Hello World",
+      "locationId": 3,
+      "id": 6
+    }
+  ]
+
+def get_all_animals():
+    return EMPLOYEES
---
+++ locations/request.py
@@ -0,0 +1,30 @@
+LOCATIONS = [
+    {
+      "id": 1,
+      "name": "Nashville North",
+      "address": "8422 Johnson Pike"
+    },
+    {
+      "id": 2,
+      "name": "Nashville South",
+      "address": "209 Emory Drive"
+    },
+    {
+      "name": "shepherdsville",
+      "address": "757 Benson Drive, apt 13",
+      "id": 3
+    },
+    {
+      "name": "shepherdsville",
+      "address": "757 Benson Drive, apt 13",
+      "id": 4
+    },
+    {
+      "name": "richmond",
+      "address": "757 Benson Drive, apt 13",
+      "id": 5
+    }
+  ]
+
+def get_all_animals():
+    return LOCATIONS
