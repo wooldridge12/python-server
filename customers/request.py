957c5195ca644ff060da8c27671ce9fb73fbc5a2
wooldridge12 <wooldridgemikey@gmail.com>
2021-07-13 15:54:21 -0400

    Finished chapter 4

---
+++ customers/request.py
@@ -0,0 +1,46 @@
+CUSTOMERS = [
+    {
+      "id": 1,
+      "name": "Hannah Hall",
+      "address": "7002 Chestnut Ct",
+      "email": "www.sdsd.com"
+    }
+]
+
+def get_all_customers():
+    '''returns customers'''
+    return CUSTOMERS
+
+    # Function with a single parameter
+def get_single_customer(id):
+    '''get single customer'''
+    # Variable to hold the found customer, if it exists
+    requested_customer = None
+
+    # Iterate the CUSTOMERS list above. Very similar to the
+    # for..of loops you used in JavaScript.
+    for customer in CUSTOMERS:
+        # Dictionaries in Python use [] notation to find a key
+        # instead of the dot notation that JavaScript used.
+        if customer["id"] == id:
+            requested_customer = customer
+
+    return requested_customer
+
+
+def create_customer(customer):
+    """Lets create Customers!!!"""
+    # Get the id value of the last customer in the list
+    max_id = CUSTOMERS[-1]["id"]
+
+    # Add 1 to whatever that number is
+    new_id = max_id + 1
+
+    # Add an `id` property to the customer dictionary
+    customer["id"] = new_id
+
+    # Add the customer dictionary to the list
+    CUSTOMERS.append(customer)
+
+    # Return the dictionary with `id` property added
+    return customer
