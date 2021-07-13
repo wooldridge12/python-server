4df4c1ce113d997060008a51fc284952f8c817c0
wooldridge12 <wooldridgemikey@gmail.com>
2021-07-13 16:29:33 -0400

    Chapter 5 animals

--- request_handler.py
+++ request_handler.py
@@ -2,7 +2,7 @@ import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from locations.request import get_all_locations, get_single_location
 from employees import get_all_employees, get_single_employee
-from animals import get_all_animals, get_single_animal, create_animal
+from animals import get_all_animals, get_single_animal, create_animal, delete_animal
 from customers import get_all_customers,get_single_customer,create_customer
 
 
@@ -118,6 +118,21 @@ class HandleRequests(BaseHTTPRequestHandler):
             new_customer = create_customer(post_body)
         # Encode the new animal and send in response
         self.wfile.write(f"{new_animal, new_customer}".encode())
+
+    def do_DELETE(self):
+    # Set a 204 response code
+        self._set_headers(204)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Delete a single animal from the list
+        if resource == "animals":
+            delete_animal(id)
+
+        # Encode the new animal and send in response
+        self.wfile.write("".encode())
+
 
     # Here's a method on the class that overrides the parent's method.
     # It handles any PUT request.
